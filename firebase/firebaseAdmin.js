71872b412718f188dcd71083383c8612f10a094c
sharugesh <sharugesh39@gmail.com>
2026-01-24 22:16:21 +0530

    Switch Firebase to env variable

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,15 +1,16 @@
 const admin = require("firebase-admin");
-const path = require("path");
 
-const serviceAccount = require(path.join(
-  __dirname,
-  "../jj-canteen-fcm-key.json"
-));
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT environment variable not set");
+}
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+  console.log("🔥 Firebase Admin initialized");
 }
 
 module.exports = admin;
